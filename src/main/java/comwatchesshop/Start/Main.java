39260f45382a92184f59252236cdf66d55efb954
sarateanu andrei <sarateanuandrei28@gmail.com>
2020-07-08 12:20:34 +0300

    methodconstructed

--- src/main/java/comwatchesshop/Start/Main.java
+++ src/main/java/comwatchesshop/Start/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 13;
         System.out.println(a);
+        if (a < 5){
+            System.out.println("Invalid");
+        }
+    }
 }
